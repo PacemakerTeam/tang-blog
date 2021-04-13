73ad83cb54ba1e08f29d54c5d75752d743c80625
hyechan <dhgpcks@gmail.com>
2021-04-13 16:41:10 +0900

    git init

---
+++ src/test/java/com/devTest/Firstblog/FirstBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devTest.Firstblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
