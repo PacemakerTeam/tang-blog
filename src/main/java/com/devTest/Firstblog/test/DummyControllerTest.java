d746f384aea9adcc9db9e64a1fa8af14a4495491
hyechan <dhgpcks@gmail.com>
2021-04-19 16:29:47 +0900

    JPA Update와 dirtyChecking

--- src/main/java/com/devTest/Firstblog/test/DummyControllerTest.java
+++ src/main/java/com/devTest/Firstblog/test/DummyControllerTest.java
@@ -8,11 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -22,6 +20,50 @@ public class DummyControllerTest {
 
     @Autowired //의존성 주입 DI
     private UserRepository userRepository;
+
+
+
+    //save함수는 id를 전달하지 않으면 insert를 해주고,
+    //save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+    //save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해줘요.
+    // email, password 수정해보자.
+    //업데이트의 경우 PutMapping을 쓴다.
+    @Transactional //시작될 때 Transaction만들어졌다가, 종류 될 때 Transaction.commit이 일어남.
+    @PutMapping("/dummy/user/{id}")//@GetMapping("/dummy/user/{id}")같아도 된다. 요청이 다르다.
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser){
+        //json 데이터를 요청 -> java Object(MessageConverter의 Jackson라이브러리가)로 변환해서 받아줘요)
+        System.out.println("id = " + id);
+        System.out.println("requestUser = " + requestUser);
+        System.out.println("passowrd = " + requestUser.getPassword());
+        System.out.println("email = " + requestUser.getEmail());
+
+        // 방법 1
+//        requestUser.setId(id);
+//        requestUser.setUsername("ssar");
+//        userRepository.save(requestUser);
+        //save를 통해서 update를 한다? 이건 새로 넣는거나 마찬가지...
+
+        // 방법 2
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+        //그래서 그냥 객체 가져와서 그거 set해서 날림...
+        //보기에는 전체를 다시 데이터 넣은 것 처럼 보이지만 아님.
+        // update처럼 작동함. 1차 캐시에 User user 스냅샷이 저장되어 있고, 바뀐 것만 update쿼리문 만들어서 날림
+
+        //방법 3  @Transactional
+        //save호출 안했는데 ? 된다.
+        //userRepository.save(user);
+
+        //더티 체킹
+        //스냅샷이랑 변경 된 데이터 감지 -> update flush -> 더티 체킹
+
+        return user;
+    }
 
     //http:localhost:8000/blog/dummy/user
     @GetMapping("/dummy/users")
